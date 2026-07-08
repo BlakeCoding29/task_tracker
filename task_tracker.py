859f875fcb3435efe02ce315cd52c650550f34ba
Your Name <your@email.com>
2026-07-08 15:21:08 -0500

    Created a basic task tracker

---
+++ task_tracker.py
@@ -0,0 +1,8 @@
+print("Task Tracker")
+
+task = input("What task do you want to track? ")
+completed = input("Did you complete it today? yes/no ")
+
+print("\nToday's Task")
+print("Task:", task)
+print("Completed:", completed)
