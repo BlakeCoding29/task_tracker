af507844fe7f4cf9caad11b8d1441437bcb592b8
Blake Slayton <wbs429@gmail.com>
2026-07-10 18:34:56 -0500

    Store multiple tasks in a list

--- task_tracker.py
+++ task_tracker.py
@@ -1,10 +1,16 @@
 print("Task Tracker")
 
-task = input("What task do you want to track? ")
-completed = input("Did you complete it today? yes/no ").lower()
+task1 = input("Enter task 1: ")
+task2 = input("Enter task 2: ")
+task3 = input("Enter task 3: ")
+completed = input("Did you complete them today? yes/no ").lower()
 
-print("\nToday's Task")
-print("Task:", task)
+tasks = [task1, task2, task3]
 
-status = "Task completed" if completed in ["yes", "y"] else "Task not completed"
+print("\nToday's Tasks")
+print("1: ", tasks[0])
+print("2: ", tasks[1])
+print("3: ", tasks[2])
+
+status = "Tasks completed" if completed in ["yes", "y"] else "Tasks not completed"
 print(status)
