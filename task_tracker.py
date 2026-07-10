d130d9b51317e49939d03edc934651da72f3a3b7
Blake Slayton <wbs429@gmail.com>
2026-07-10 00:16:57 -0500

    Improve input handling with lists and lower()

--- task_tracker.py
+++ task_tracker.py
@@ -1,7 +1,7 @@
 print("Task Tracker")
 
 task = input("What task do you want to track? ")
-completed = input("Did you complete it today? yes/no ")
+completed = input("Did you complete it today? yes/no ").lower()
 
 print("\nToday's Task")
 print("Task:", task)
