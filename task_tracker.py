4cff05edbf493b063659b065917ba81ea67440d2
Blake Slayton <wbs429@gmail.com>
2026-07-10 02:04:08 -0500

    Use list membership for completion response

--- task_tracker.py
+++ task_tracker.py
@@ -6,7 +6,7 @@ completed = input("Did you complete it today? yes/no ").lower()
 print("\nToday's Task")
 print("Task:", task)
 
-if completed == "yes" or completed == "y":
+if completed in ["yes", "y"]:
     print("Task completed")
 else:
     print("Task not completed")
